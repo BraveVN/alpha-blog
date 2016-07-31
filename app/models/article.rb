21ef650c886d9ac49e18dc0577f0f9de141c25b5
Dung Pham <dung.pham.11tclc@gmail.com>
2016-07-31 04:23:33 +0000

    Add association between user and articles and add debugger view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ActiveRecord::Base
+    belongs_to :user
     validates :title, presence: true, length: { minimum: 3, maximum: 50 }    # ensure title not nil
     validates :description, presence: true, length: { minimum: 10, maximum: 300 }    # ensure description not nil
+    validates :user_id, presence: true
 end
